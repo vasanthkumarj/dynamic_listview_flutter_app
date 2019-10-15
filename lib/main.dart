652bbed25851e3d6458399fed46d85dc56aaea00
vasanth <vasanthkumar.j@outlook.com>
2019-10-15 10:03:09 +0530

    added FAB and snack bar

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,29 @@ void main()
                 home: Scaffold(
                   appBar: AppBar(title: Text("Dynamic list"),),
                   body: getListView(),
+                  floatingActionButton: FloatingActionButton(
+                                                           onPressed: (){
+                                                             debugPrint("clicked FAB");
+                                                           },
+                                                           child: Icon(Icons.add)
+                                                            ),
                 ),
                  )
 
   );
+}
+
+void showSnackBar(BuildContext context, String item)
+{
+  var snack_bar = SnackBar(content: Text("You tapped $item"),
+                          duration: const Duration(milliseconds: 5000),
+                          action: SnackBarAction(
+                             label: "UNDO",
+                             onPressed: (){
+                                    debugPrint("pressed UNDO");
+                             },
+                          ),);
+  Scaffold.of(context).showSnackBar(snack_bar);
 }
 
 
@@ -29,7 +48,13 @@ Widget getListView()
       itemCount: listItems.length,
       itemBuilder: (context, index)
       {
-        return ListTile(title: Text(listItems[index]),);
+        return ListTile(
+          title: Text(listItems[index]),
+          onTap: ()
+          {
+            showSnackBar(context, listItems[index]);
+          }
+                       );
       }
   );
 
